869807476cb4cf081b74bf4534f20c3d1d43272d
Lei Wang <tendant@gmail.com>
2016-03-07 23:14:03 -0800

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to graphql-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/graphql_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns graphql-clj.core-test
+  (:require [clojure.test :refer :all]
+            [graphql-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
