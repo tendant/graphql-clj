0ffe2541d3971054480031ef89c6fd0a8f65f560
Lei Wang <tendant@gmail.com>
2016-03-08 14:54:38 -0800

    fix alias

--- src/graphql_clj/parser.clj
+++ src/graphql_clj/parser.clj
@@ -52,4 +52,55 @@
     }
   }
 }"
+
+  "# `me` could represent the currently logged in viewer.
+{
+  me {
+    name
+  }
+}
+"
+
+  "# `me` could represent the currently logged in viewer.
+{
+  me {
+    name
+  }
+}
+
+# `user` represents one of many users in a graph of data, referred to by a
+# unique identifier.
+{
+  user(id: 4) {
+    name
+  }
+}"
+
+  "{
+  user(id: 4) {
+    id
+    name
+    profilePic(size: 100)
+  }
+}
+"
+  "{
+  user(id: 4) {
+    id
+    name
+    profilePic(width: 100, height: 50)
+  }
+}
+"
+
+  ;; In this example, we can fetch two profile pictures of different sizes and ensure the resulting object will not have duplicate keys:
+  "{
+  user(id: 4) {
+    id
+    name
+    smallPic: profilePic(size: 64)
+    bigPic: profilePic(size: 1024)
+  }
+}
+"
   )
