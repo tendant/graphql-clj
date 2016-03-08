9ad05051d9357570a25eb095cfb235ce385771bc
Lei Wang <tendant@gmail.com>
2016-03-08 13:21:39 -0800

    wip: parser

--- src/graphql_clj/parser.clj
+++ src/graphql_clj/parser.clj
@@ -1,3 +1,14 @@
 (ns graphql-clj.parser
   (:require [instaparse.core :as insta]
             [clojure.java.io :as io]))
+
+(comment
+  ((insta/parser (io/resource "graphql.bnf")) "query {
+  user(id: 4) {
+    id
+    name
+    profilePic(width: 100, height: 50)
+  }
+}
+")
+  )
