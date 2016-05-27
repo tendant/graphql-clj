113ea361e9b5574c7ddaebcd829ab45975b5ece9
Lei Wang <tendant@gmail.com>
2016-05-26 20:30:15 -0700

    add rhizome for graph visualization

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [instaparse "1.4.1"]])
+                 [instaparse "1.4.1"]
+                 [rhizome "0.2.5"]])
