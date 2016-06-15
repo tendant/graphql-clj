40174cab9d7789ce21fc8badf126a8b77ed5c974
Lei Wang <tendant@gmail.com>
2016-06-15 14:41:44 -0700

    add parser_test.clj

--- src/graphql_clj/parser_test.clj
+++
@@ -1,5 +0,0 @@
-(ns graph-clj.parser-test
-  (:require [clojure.test :refer :all]
-            [graphql-clj.parser :refer :all]))
-
-(deftest )
--- test/graphql_clj/core_test.clj
+++ test/graphql_clj/core_test.clj
@@ -3,5 +3,5 @@
             [graphql-clj.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "FIXME: fixed"
+    (is (= 0 0))))
