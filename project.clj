bba5d847c1c2e74f784edda52aff8a6e22d556b9
Lei Wang <tendant@gmail.com>
2016-07-12 13:35:01 -0700

    add s3-wagon-private plugin

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
                  [instaparse "1.4.1"]
                  [com.taoensso/timbre "4.3.1"]
                  [rhizome "0.2.5"]]
+  :plugins [[s3-wagon-private "1.2.0"]]
   :repositories [["private" {:url "s3p://eng-repos/libs/releases/"
                              :creds :gpg}]])
