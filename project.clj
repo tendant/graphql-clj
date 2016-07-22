936f784a281ad798c062fdcf92bbb53082df7a54
Lei Wang <tendant@gmail.com>
2016-07-21 17:07:08 -0700

    add core.match for resolver-fn

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [instaparse "1.4.1"]
                  [com.taoensso/timbre "4.3.1"]
                  [rhizome "0.2.5"]]
-  :profiles {:dev {:dependencies [[io.forward/yaml "1.0.3"]]}}
+  :profiles {:dev {:dependencies [[io.forward/yaml "1.0.3"]
+                                  [org.clojure/core.match "0.3.0-alpha4"]]}}
   :plugins [[s3-wagon-private "1.1.2"]]
   :repositories [["private" {:url "s3p://eng-repos/libs/releases/"
                              :creds :gpg}]])
--- test/graphql_clj/executor_test.clj
+++ test/graphql_clj/executor_test.clj
@@ -2,12 +2,15 @@
   (:use graphql-clj.executor)
   (:require [clojure.test :refer :all]
             [graphql-clj.type :as type]
-            [graphql-clj.parser :as parser]))
+            [graphql-clj.parser :as parser]
+            [clojure.core.match :as match]))
 
 (def simple-user-schema
   "type User {
   name: String
   nickname: String
+  son: User
+  friends: [User]
 }
 
 type QueryRoot {
@@ -20,13 +23,17 @@ schema {
 
 (def resolver-fn 
   (fn [type-name field-name]
-    (cond
-      (and (= type-name "QueryRoot") (= field-name "user")) (fn [& args]
-                                                              {:name "Test user name"
-                                                               :nickname "Test user nickname"})
-      :else (fn [context parent & args]
-              (println (format "Not found resolver function for type(%s) and field(%s)." type-name field-name))
-              (get parent (keyword field-name))))))
+    (match/match
+     [type-name field-name]
+     ["QueryRoot"  "user"] (fn [& args]
+                             {:name "Test user name"
+                              :nickname "Test user nickname"})
+     ["User" "son"] (fn [context parent & args]
+                      {:name "Test son name"
+                       :nickname "Test son nickname"})
+     :else (fn [context parent & args]
+             (println (format "Not found resolver function for type(%s) and field(%s)." type-name field-name))
+             (get parent (keyword field-name))))))
 
 (deftest test-simple-execution
   (testing "test simple execution"
@@ -40,7 +47,10 @@ schema {
           query-operation (first (:operation-definitions document))
           query-selection-set (:selection-set query-operation)
           user-selection (first query-selection-set)
-          user-selection-set (get-in (second user-selection) [:field :selection-set])]
+          user-selection-set (get-in (second user-selection) [:field :selection-set])
+          new-document (parser/transform (parser/parse "query {user {name son}}"))
+          new-result (execute context schema resolver-fn new-document)
+          ]
       (is (= "user" (get-selection-name user-selection)))
       (is (= :field (get-selection-type user-selection)))
       (is (= user-selection-set (get-field-selection-set user-selection)))
