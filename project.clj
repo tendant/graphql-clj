f9991f013e3e3956514bf215ea23eb79cc66e4db
Lei Wang <tendant@gmail.com>
2016-07-22 21:08:41 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject graphql-clj "0.1.4"
+(defproject graphql-clj "0.1.5-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
