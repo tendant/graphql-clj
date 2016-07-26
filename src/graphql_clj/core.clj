a22c3c30fff280f20b16bf4386a80446873511db
Lei Wang <tendant@gmail.com>
2016-07-26 11:17:50 -0700

    remove core.clj and core_test.clj

--- src/graphql_clj/core.clj
+++
@@ -1,6 +0,0 @@
-(ns graphql-clj.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/graphql_clj/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns graphql-clj.core-test
-  (:require [clojure.test :refer :all]
-            [graphql-clj.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME: fixed"
-    (is (= 0 0))))
