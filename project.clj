c86c74d0c0223c66402d6c9327052e914394d9e9
Lei Wang <tendant@gmail.com>
2016-07-26 11:38:54 -0700

    update dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [instaparse "1.4.1"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
                  [com.taoensso/timbre "4.3.1"]]
-  :profiles {:dev {:dependencies [[io.forward/yaml "1.0.3"]
-                                  [org.clojure/core.match "0.3.0-alpha4"]
-                                  [rhizome "0.2.5"]]}}
-  :plugins [[s3-wagon-private "1.1.2"]]
-  )
+  :profiles {:dev {:dependencies [[io.forward/yaml "1.0.3"]]}})
