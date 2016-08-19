b705e943147a69147ad93004a5a9b9aa7aa71893
Lei Wang <tendant@gmail.com>
2016-08-19 12:59:42 -0700

    upgrade clojure to 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/tendant/graphql-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [instaparse "1.4.1"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [com.taoensso/timbre "4.3.1"]]
