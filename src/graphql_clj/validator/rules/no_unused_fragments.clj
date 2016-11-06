fd33ca202aabf4bc07670e2d442ea6db186ea5c9
Edward Wible <edward.wible@nubank.com.br>
2016-11-06 14:49:34 -0200

    Support loc for no unused fragments

--- src/graphql_clj/validator/rules/no_unused_fragments.clj
+++ src/graphql_clj/validator/rules/no_unused_fragments.clj
@@ -3,17 +3,19 @@
    within operations, or spread within other fragments spread within operations."
   (:require [graphql-clj.visitor :refer [defnodevisitor]]
             [graphql-clj.validator.errors :as ve]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [graphql-clj.box :as box]))
 
 (defn- unused-fragment-error [{:keys [operation-type]} fragment-name]
-  (if (:name operation-type)
-    (format "Fragment '%s' is never used in operation '%s'." fragment-name (:name operation-type))
-    (format "Fragment '%s' is never used." fragment-name)))
+  {:error (if (:name operation-type)
+            (format "Fragment '%s' is never used in operation '%s'." fragment-name (:name operation-type))
+            (format "Fragment '%s' is never used." fragment-name))
+   :loc (ve/extract-loc (meta fragment-name))})
 
 (defnodevisitor fragment-def :pre :fragment-definition
   [{:keys [name] :as n} s]
   (when name
-    {:state (update s :frag-defs conj name)}))
+    {:state (update s :frag-defs conj (box/->Box name (meta n)))}))
 
 (defnodevisitor fragment-use :pre :fragment-spread
   [{:keys [name] :as n} s]
