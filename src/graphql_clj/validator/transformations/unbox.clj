6704bfea9f24e1d779ffe76c98dbdbc1060ea05c
Edward Wible <edward.wible@nubank.com.br>
2016-11-12 16:33:33 -0200

    Extract unbox node function

--- src/graphql_clj/validator/transformations/unbox.clj
+++ src/graphql_clj/validator/transformations/unbox.clj
@@ -2,19 +2,21 @@
   (:require [graphql-clj.visitor :as v]
             [graphql-clj.box :as box]))
 
+(defn- unbox-node [n]
+  (cond-> n
+          (:type-name n)      (update :type-name     box/box->val)
+          (:field-name n)     (update :field-name    box/box->val)
+          (:argument-name n)  (update :argument-name box/box->val)
+          (:variable-name n)  (update :variable-name box/box->val)
+          (:implements n)     (update-in [:implements :type-names] #(mapv box/box->val %))
+          (:type-condition n) (update-in [:type-condition :type-name] box/box->val)
+          (:type-names n)     (update :type-names #(mapv box/box->val %))
+          (:inner-type n)     (update-in [:inner-type :type-name] box/box->val)
+          (:on n)             (update-in [:on :type-name] box/box->val)
+          (:value n)          (update :value box/box->val)))
+
 (declare unbox)
 (v/defmapvisitor unbox :post [n s]
-  (when (:node-type n)
-    (cond-> {:node n}
-            (:type-name n)      (update-in [:node :type-name]     box/box->val)
-            (:field-name n)     (update-in [:node :field-name]    box/box->val)
-            (:argument-name n)  (update-in [:node :argument-name] box/box->val)
-            (:variable-name n)  (update-in [:node :variable-name] box/box->val)
-            (:implements n)     (update-in [:node :implements :type-names] #(mapv box/box->val %))
-            (:type-condition n) (update-in [:node :type-condition :type-name] box/box->val)
-            (:type-names n)     (update-in [:node :type-names] #(mapv box/box->val %))
-            (:inner-type n)     (update-in [:node :inner-type :type-name] box/box->val)
-            (:on n)             (update-in [:node :on :type-name] box/box->val)
-            (:value n)          (update-in [:node :value] box/box->val))))
+  (when (:node-type n) {:node (unbox-node n)}))
 
 (def rules [unbox])
