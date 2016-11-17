767d27f786341062f4ddbc8b54519f027c38e59d
Edward Wible <edward.wible@nubank.com.br>
2016-11-17 03:52:05 -0200

    Remove comments

--- src/graphql_clj/visitor.clj
+++ src/graphql_clj/visitor.clj
@@ -130,7 +130,7 @@
         (assoc :state (-> result :document section :state))
         (update-in [:document section] dissoc :state))))
 
-(defn- schema-definition ;; TODO extract to validator
+(defn- schema-definition
   "Given a parsed schema document, return the schema definition that defines the entry point
    types (schema and optionally mutation). Can return nil, in which case the default type
    `Query` will be used and mutations will be disabled."
@@ -140,7 +140,7 @@
            (filter #(= :schema-definition (:node-type %)))
            first))
 
-(defn- upsert-schema-entrypoint ;; TODO extract to validator
+(defn- upsert-schema-entrypoint
   [type-system-definitions root-query-name]
   (let [grouped (group-by #(= root-query-name (:type-name %)) type-system-definitions)
         entrypoint (intro/upsert-root-query (first (get grouped true)) root-query-name)
@@ -158,7 +158,7 @@
 (defmethod of-type :default [{:keys [type-name]}]
   type-name)
 
-(defn- root-fields ;; TODO extract to validator
+(defn- root-fields
   "Given a parsed schema document, return [root-name {root-field Type}]
    When validating queries, we need to know the types of the root fields to map to the same specs
    that we registered when parsing the schema.  Similar for mutations."
@@ -195,7 +195,7 @@
   [sym type node-type bindings & body]
   `(def ~sym (nodevisitor ~type ~node-type ~bindings ~@body)))
 
-(defn initial-state [schema]                                ;; TODO extract to validator
+(defn initial-state [schema]
   (let [{:keys [mutation-type] :as schema-definition} (schema-definition schema)
         schema-definition (or schema-definition {:node-type :schema-definition :query-type {:name "Query"}})
         root-query-name   (get-in schema-definition [:query-type :name])
