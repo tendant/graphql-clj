75bd60f2dc77018fc047ab2c09c9af73fa74e631
Edward Wible <edward.wible@nubank.com.br>
2016-11-20 17:09:09 -0200

    Remove unused functions from inline types

--- src/graphql_clj/validator/transformations/inline_types.clj
+++ src/graphql_clj/validator/transformations/inline_types.clj
@@ -1,20 +1,7 @@
 (ns graphql-clj.validator.transformations.inline-types
   "Inline field and parent field types for execution phase"
   (:require [graphql-clj.visitor :as v]
-            [graphql-clj.spec :as spec]
-            [clojure.spec :as s]
             [graphql-clj.box :as box]))
-
-(defn- of-kind [{:keys [kind required inner-type]} s]
-  (if (:type-name inner-type)
-    (let [base (spec/get-type-node (spec/named-spec s [(:type-name inner-type)]) s)]
-      (select-keys base [:kind :required]))
-    {:kind kind :required required :of-kind (of-kind inner-type s)}))
-
-(defn- parent-type [{:keys [spec]}]
-  (if-let [base-spec (s/get-spec spec)]
-    (if (keyword? base-spec) base-spec spec)
-    spec))
 
 (def whitelisted-keys
   #{:v/parentk :node-type :selection-set :type-name :field-name :name :args-fn :kind :of-kind :required :parent-type-name})
