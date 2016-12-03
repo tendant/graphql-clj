cde6d652e3181d724ddf25125fb2df8e385bf865
Lei Wang <tendant@gmail.com>
2016-12-03 14:42:07 -0800

    wip nodes

--- src/graphql_clj/validation.clj
+++ src/graphql_clj/validation.clj
@@ -4,31 +4,21 @@
             [zip.visit :as zv]
             [graphql-clj.parser :as parser]))
 
-(defn- verify-node [node ancestor]
-  (if (= :type-definition (:node-type node))
-    (do
-      (println "type-definition:", (:node-type node))
-      node)
-    node))
-
-(defn validate-query-document
-  [query-document]
-  (let [ancestor (atom [])])
-  (walk/prewalk verify-node query-document))
-
 ;; Zipper
 
 (defn node->branch? [node]
   (case (:node-type node)
     :schema-definition false
     :type-definition true
-    (println "error: unknow node-type for branch:" (:node-type node))))
+    :type-field true
+    (println "error: unknown node-type for branch:" (:node-type node) "," node)))
 
 (defn make-node-with-node-type
   [node children]
   (case (:node-type node)
     ;; :operation-definition (assoc node :selection-set children)
     :type-definition (assoc node :fields children :changed "visited")
+    :type-field (assoc node :fields children :changed "visited")
     (println "error: don't know how to create node for:" (:node-type node))))
 
 (defn node->children
@@ -36,16 +26,17 @@
   (println "node->children:" (:node-type node))
   (case (:node-type node)
     :type-definition (:fields node)
+    :type-field (:fields node)
     (println "error: don't know to make children from:" node)))
 
 (defn branch? [node]
-  (println "branch?")
+  (println "branch?" (keys node))
   (cond
     (:operation-definitions node) true
     (:type-system-definitions node) true
     (:node-type node) (node->branch? node)
     :default (do
-               (println "error no branch for node:" node)
+               (println "error: don't know how to create branch for node:" node)
                false)))
 
 (defn make-node-fn
@@ -58,12 +49,12 @@
       (:operation-definitions node) (assoc node :operation-definitions children)
       (:type-system-definitions node) (assoc node :type-system-definitions children)
       (:node-type node) (make-node-with-node-type node children)
-      :default node)
+      :default (println "error: node is not a map!" node))
     (println "error: don't know how to make node from:" node "--- map?:" (map? node))))
 
 (defn children
   [node]
-  (println "children")
+  (println "children:" (keys node))
   (cond
     (:operation-definitions node) (do
                                     (:operation-definitions node))
@@ -77,8 +68,8 @@
   [document]
   (z/zipper branch? children make-node-fn document))
 
-(defn debug [& args]
-  (println "args:" args))
+(defn debug [action node state]
+  (println "debug:" action))
 
 (defn visit
   [document]
