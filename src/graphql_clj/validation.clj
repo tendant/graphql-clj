f502373789931afeb027d0fbd103ff902b564d67
Lei Wang <tendant@gmail.com>
2016-12-14 02:58:56 -0800

    update test schema

--- src/graphql_clj/validation.clj
+++ src/graphql_clj/validation.clj
@@ -112,23 +112,47 @@
   (assert (s/conform :graphql-clj/query parsed-query)))
 
 (comment
-  (def schema-str "schema {
-  query: Query
+  (def schema-str "enum DogCommand { SIT, DOWN, HEEL }
+
+type Dog implements Pet {
+  name: String!
+  nickname: String
+  barkVolume: Int
+  doesKnowCommand(dogCommand: DogCommand!): Boolean!
+  isHousetrained(atOtherHomes: Boolean): Boolean!
+  owner: Human
 }
 
-type Query {
-  a: A
-  x: X
+interface Sentient {
+  name: String!
 }
 
-type A {
-  b: B
+interface Pet {
+  name: String!
 }
 
-type B {
-  c(foo: Int): String
+type Alien implements Sentient {
+  name: String!
+  homePlanet: String
 }
 
-type X {
-  y(foo: Int): String
+type Human implements Sentient {
+  name: String!
+}
+
+enum CatCommand { JUMP }
+
+type Cat implements Pet {
+  name: String!
+  nickname: String
+  doesKnowCommand(catCommand: CatCommand!): Boolean!
+  meowVolume: Int
+}
+
+union CatOrDog = Cat | Dog
+union DogOrHuman = Dog | Human
+union HumanOrAlien = Human | Alien
+
+type QueryRoot {
+  dog: Dog
 }"))
