c0928401a7fe0f3547d66041f6e3c68418bb7437
Jeff Ichnowski <jeff.ichnowski@gmail.com>
2017-03-27 22:21:28 -0400

    Added start of query validator, still many things to check, but some basics are there.

---
+++ src/graphql_clj/query_validator.clj
@@ -0,0 +1,157 @@
+(ns graphql-clj.query-validator
+  (:require [graphql-clj.parser :as parser]
+            [graphql-clj.schema-validator :as schema-validator]
+            [clojure.set :as set]
+            [clojure.pprint :refer [pprint]]))
+
+;; TODO: share with schema validator
+(defn- err [errors node fmt & args]
+  (let [m (meta node)]
+    (conj errors {:message (apply format fmt args)
+                  :start {:line (:instaparse.gll/start-line m)
+                          :column (:instaparse.gll/start-column m)
+                          :index (:instaparse.gll/start-index m)}
+                  :end {:line (:instaparse.gll/end-line m)
+                        :column (:instaparse.gll/end-column m)
+                        :index (:instaparse.gll/end-index m)}})))
+
+(defn- base-type [type]
+  (case (:tag type)
+    :basic-type (:name type)
+    :list-type (recur (:inner-type type))))
+
+(defn- get-root [schema def]
+  (get (:roots schema) (if (= :mutation-definition (:tag def)) :mutation :query)))
+
+(defn- check-selection-set
+  [schema errors fragmap tname sset]
+  (let [tdef (get-in schema [:type-map tname])
+        fieldmap (:field-map tdef)]
+    (if (nil? (:selection-set sset))
+      [errors sset]
+      (loop [errors errors vfields [] [f & fields] (:selection-set sset)]
+        (if f
+          (case (:tag f)
+            :selection-field
+            (if-let [fdecl (fieldmap (:name f))]
+              (let [[errors f] (check-selection-set schema errors fragmap (base-type (:type fdecl)) f)]
+                (recur errors (conj vfields (assoc f :resolved-type (:type fdecl))) fields))
+              (recur (if (:alias f)
+                       (err errors (:name f) "field '%s' (aliased as '%s') is not defined on type '%s'" (:name f) (:alias f) tname)
+                       (err errors (:name f) "field '%s' is not defined on type '%s'" (:name f) tname))
+                     vfields
+                     fields))
+
+            :inline-fragment
+            (let [on (get-in f [:on :name])]
+              (if-let [ontype (get-in schema [:type-map on])]
+                (case (:tag ontype)
+                  (:type-definition :interface-definition :union-definition)
+                  (recur errors vfields fields) ;; TODO: check-selection-set recurse
+                  (recur (err errors on "inline fragment on non-composite type '%s'" on) vfields fields))
+                (recur (err errors on "inline fragment on undefined type '%s'" on) vfields fields)))
+             
+            :fragment-spread
+            (if-let [frag (fragmap (:name f))]
+              (recur errors vfields fields)
+              (recur (err errors (:name f) "fragment '%s' is not defined" (:name f)) vfields fields)))
+          [errors (assoc sset :selection-set vfields)])))))
+
+(defn- build-fragment-map [errors schema query]
+  (loop [errors errors fmap {} [def & defs] (filter #(= :fragment-definition (:tag %)) query)]
+    (cond
+      (nil? def) [errors fmap]
+      (contains? fmap (:name def)) (recur (err errors (:name def) "fragment '%s' is already declared" (:name def)) fmap defs)
+      :else (recur errors (assoc fmap (:name def) def) defs))))
+
+;; Compute the referenced fragment set from a given selection-set.
+;; The result is a hash set of symbols of fragments referenced by the
+;; selection set argument.
+(defn- selection-set-fragment-refs [sset]
+  (-> (fn accum [refs f]
+        (case (:tag f)
+          :selection-field refs
+          :fragment-spread (conj! refs (:name f))
+          :inline-fragment (reduce accum refs (:selection-set f))))
+      (reduce (transient #{}) sset)
+      (persistent!)))
+
+;; Build a map from fragment name to referenced fragments. The key is
+;; a symbol from the fragment name, the value is a set of referenced
+;; fragments.
+(defn- build-fragment-ref-graph [fmap]
+  (reduce-kv #(assoc %1 %2 (selection-set-fragment-refs (:selection-set %3))) {} fmap))
+
+;; Expand references in a graph.  g is a map from 'name to
+;; #{'references}.  refs is a set of references that need to be
+;; expanded.  E.g. g = {:a #{b c} :b #{c} :c #{}}, and refs = #{b c}, then
+;; the result will be union of #{c} and #{}, thus #{c}.
+(defn- expand-refs [g refs]
+  (reduce set/union #{} (map g refs)))
+
+(defn- check-fragment-cycles [errors fmap]
+  (loop [errors errors g (build-fragment-ref-graph fmap)]
+    (if (empty? g)
+      errors
+      ;; recurrance:
+      ;; (1) cyclic reference is detected when a fragment references
+      ;; itself after transitive dependencies are added.
+      ;; (2) the graph is updated to remove all nodes with no
+      ;; references remaining (thus cannot participate in a cyclic
+      ;; reference), or when the error for the cyclic reference has
+      ;; been recorded.  The map is then updated to add dependencies
+      (recur (->> (filter #(contains? (% 1) (% 0)) g)
+                  (reduce #(err %1 (%2 0) "fragment '%s' contains a cyclic reference" (%2 0)) errors))
+             (->> (remove (fn [[k v]] (or (contains? v k) (empty? v))) g)
+                  (reduce (fn [g' [k v]]
+                              (->> (map g v) ;; map a dependency to its frag reference sets
+                                   (reduce set/union v) ;; union them together
+                                   (filter g) ;; remove dependencies no longer in graph
+                                   (into #{}) ;; put it back into a set, then assoc it to the updated graph
+                                   (assoc g' k))) {}))))))
+
+(defn- check-operations [errors schema fmap query]
+  (loop [errors errors vdefs [] anon nil dmap {} fmap' fmap [def & defs] query]
+    (if def
+      (case (:tag def)
+        :selection-set
+        (if anon
+          ;; 5.1.2.1 lone anonymous operation
+          (recur (err errors def "anonymous selection set is already declared") vdefs anon dmap fmap' defs)
+          (let [[errors def] (check-selection-set schema errors fmap (get-in schema [:roots :query]) def)] 
+            (recur errors (conj vdefs def) def dmap fmap' defs)))
+
+        (:mutation :query-definition)
+        (if (contains? dmap (:name def))
+          ;; 5.1.1.1 operation name uniqueness
+          (recur (err errors (:name def) "operation with name '%s' is already declared" (:name def)) vdefs anon dmap fmap' defs)
+          (let [[errors def] (check-selection-set schema errors fmap (get-root schema def) def)]
+            (recur errors (conj vdefs def) anon (assoc dmap (:name def) def) fmap' defs)))
+
+        :fragment-definition
+        (let [on (get-in def [:on :name])]
+          (if-let [t (get-in schema [:type-map on])]
+            (if (#{:type-definition :interface-definition :union-definition} (:tag t))
+              (let [[errors def] (check-selection-set schema errors fmap on def)]
+                (recur errors vdefs anon dmap (assoc fmap' (:name def) def) defs))
+              ;; 5.4.1.3 Fragments on composite types
+              (recur (err errors on "fragment on non-composite type '%s'" on) vdefs anon dmap fmap' defs))
+            (recur (err errors on "fragment on undefined type '%s'" on) vdefs anon dmap fmap' defs))))
+
+      ;; else, end of defs:
+      ;; 5.1.2.1 lone anonymous operation
+      [(if (and anon (not (empty? dmap)))
+         (err errors anon "anonymous selection set must be lone operation")
+         errors)
+       vdefs])))
+  
+(defn validate-query
+  [schema query]
+  (let [[errors fmap] (build-fragment-map [] schema query)
+        errors (check-fragment-cycles errors fmap)
+        [errors vquery] (check-operations errors schema fmap query)]
+    (if (empty? errors)
+      vquery
+      (throw (ex-info "Query document failed validation"
+                      {:query query
+                       :errors errors})))))
---
+++ test/graphql_clj/query_validator_test.clj
@@ -0,0 +1,318 @@
+(ns graphql-clj.query-validator-test
+  (:require [clojure.test :refer :all]
+            [graphql-clj.parser :as parser]
+            [graphql-clj.schema-validator :as schema-validator]
+            [graphql-clj.query-validator :as query-validator]))
+
+(def ^:private example-schema
+  (->> "enum DogCommand { SIT, DOWN, HEEL }
+
+type Dog implements Pet {
+  name: String!
+  nickname: String
+  barkVolume: Int
+  doesKnowCommand(dogCommand: DogCommand!): Boolean!
+  isHousetrained(atOtherHomes: Boolean): Boolean!
+  owner: Human
+}
+
+interface Sentient {
+  name: String!
+}
+
+interface Pet {
+  name: String!
+}
+
+type Alien implements Sentient {
+  name: String!
+  homePlanet: String
+}
+
+type Human implements Sentient {
+  name: String!
+}
+
+enum CatCommand { JUMP }
+
+type Cat implements Pet {
+  name: String!
+  nickname: String
+  doesKnowCommand(catCommand: CatCommand!): Boolean!
+  meowVolume: Int
+}
+
+union CatOrDog = Cat | Dog
+union DogOrHuman = Dog | Human
+union HumanOrAlien = Human | Alien
+
+type QueryRoot {
+  dog: Dog
+}"
+       (parser/parse-schema)
+       (schema-validator/validate-schema)
+       peek))
+
+(defn- err [msg sl sc si el ec ei]
+  {:message msg :start {:line sl :column sc :index si} :end {:line el :column ec :index ei}})
+
+(defmacro deftest-valid [name schema query expected]
+  `(deftest ~name
+     (let [expect# ~expected
+           actual# (->> (parser/parse-query-document ~query)
+                       (query-validator/validate-query ~schema))]
+       (if (= expect# actual#)
+         (report {:type :pass})
+         (report {:type :fail :expected expect# :actual actual#})))))
+
+(defmacro deftest-invalid [name schema query & errors]
+  `(deftest ~name
+     (try (->> (parser/parse-query-document ~query)
+               (query-validator/validate-query ~schema))
+          (report {:type :fail :message "Expected an exception"})
+          (catch Exception ex#
+            (if-let [~'errors (:errors (ex-data ex#))]
+              (is (~'= ~'errors [~@errors]))
+              (report {:type :fail :expected "validation exception" :actual ex#}))))))
+  
+
+(deftest-valid sec-5-1-1-1-operation-name-uniqueness-valid example-schema
+  "query getDogName {
+     dog {
+       name
+     }
+   }"
+  [{:tag :query-definition
+    :name 'getDogName
+    :selection-set
+    [{:tag :selection-field
+      :name 'dog
+      :resolved-type {:tag :basic-type, :name 'Dog}
+      :selection-set
+      [{:tag :selection-field
+        :name 'name
+        :resolved-type {:tag :basic-type, :name 'String, :required true}}]}]}])
+
+(deftest-valid sec-5-1-1-1-operation-name-uniqueness-valid-2 example-schema
+  "query getOwnerName {
+     dog {
+       owner {
+         name
+       }
+     }
+   }"
+  [{:tag :query-definition
+    :name 'getOwnerName
+    :selection-set
+    [{:tag :selection-field
+      :name 'dog
+      :resolved-type {:tag :basic-type, :name 'Dog}
+      :selection-set
+      [{:tag :selection-field
+        :name 'owner
+        :resolved-type {:tag :basic-type, :name 'Human}
+        :selection-set
+        [{:tag :selection-field, :name 'name,
+          :resolved-type {:tag :basic-type, :name 'String, :required true}}]}]}]}])
+
+(deftest-invalid sec-5-1-1-1-operation-name-uniqueness-invalid-1 example-schema
+  "query getName {
+     dog {
+       name
+     }
+   }
+
+   query getName {
+     dog {
+       owner {
+         name
+       }
+     }
+   }"
+  (err "operation with name 'getName' is already declared" 7 10 61 7 17 68))
+
+(deftest-invalid sec-5-1-1-1-operation-name-uniqueness-invalid-2 example-schema
+  "query dogOperation {
+     dog {
+       name
+     }
+   }
+
+   mutation dogOperation {
+     mutateDog {
+       id
+     }
+   }"
+  (err "operation with name 'dogOperation' is already declared" 7 13 69 7 25 81))
+
+(deftest-invalid sec-5-1-2-1-lone-anonymous-operation-invalid example-schema
+  "{
+  dog {
+    name
+  }
+}
+
+query getName {
+  dog {
+    owner {
+      name
+    }
+  }
+}"
+  (err "anonymous selection set must be lone operation" 1 1 0 5 2 24))
+
+
+(deftest-invalid sec-5-1-2-1-lone-anonymous-operation-invalid-2 example-schema
+  "{ dog { name } } { dog { owner { name } } }"
+  (err "anonymous selection set is already declared" 1 18 17 1 44 43))
+
+;; Check that :type is added to dog field
+(deftest-valid sec-5-2-1-field-selection-on-objects-interfaces-and-union-types-valid example-schema
+  "{ dog }"
+  [{:tag :selection-set
+    :selection-set [{:tag :selection-field
+                     :name 'dog
+                     ;; the :resolve-type field is associated to the query
+                     ;; during validation.
+                     :resolved-type {:tag :basic-type :name 'Dog}}]}])
+
+(deftest-valid sec-5-1-2-1-lone-anonymous-operation-valid example-schema
+  "{
+    dog {
+      name
+    }
+  }"
+  ;; this checks also that fields are recursively checked and
+  ;; that :resolved-type fields are associated.
+  [{:tag :selection-set
+    :selection-set
+    [{:tag :selection-field
+      :name 'dog
+      :resolved-type {:tag :basic-type :name 'Dog}
+      :selection-set
+      [{:tag :selection-field
+        :name 'name
+        :resolved-type
+        {:tag :basic-type
+         :name 'String
+         :required true}}]}]}])
+
+(deftest-invalid sec-5-2-1-field-selection-on-objects-interfaces-and-union-types-1 example-schema
+  "{ cat }"
+  (err "field 'cat' is not defined on type 'QueryRoot'" 1 3 2 1 6 5))
+
+(deftest-invalid sec-5-2-1-field-selection-on-objects-interfaces-and-union-types-1b example-schema
+  "query getCat { cat }"
+  (err "field 'cat' is not defined on type 'QueryRoot'" 1 16 15 1 19 18))
+
+(deftest-invalid sec-5-2-1-field-selection-on-objects-interfaces-and-union-types-2 example-schema
+  "{ dog { meowVolume } }"
+  (err "field 'meowVolume' is not defined on type 'Dog'" 1 9 8 1 19 18))
+
+(deftest-invalid sec-5-2-1-field-selection-on-objects-interfaces-and-union-types-3 example-schema
+  "{ dog: cat }"
+  (err "field 'cat' (aliased as 'dog') is not defined on type 'QueryRoot'" 1 8 7 1 11 10))
+
+;; TODO: 5.2.1
+
+
+;; 5.4.1.1 Fragment name uniqueness
+(deftest-invalid fragment-name-uniqueness example-schema
+  "{ dog { ...frag1 } }
+   fragment frag1 on Dog { name }
+   fragment frag1 on Dog { name }"
+  (err "fragment 'frag1' is already declared" 3 13 67 3 18 72))
+
+(deftest-invalid fragment-on-undefined-type example-schema
+  "{ dog { ...nameFrag } }
+   fragment nameFrag on Foo {
+     name
+   }"
+  (err "fragment on undefined type 'Foo'" 2 25 48 2 28 51))
+
+;; 5.4.1.3 Fragments on composite types
+(deftest-invalid fragment-on-composite-type-1 example-schema
+  "{ dog }
+   fragment fragOnScalar on Int {
+     something
+   }"
+  (err "fragment on non-composite type 'Int'" 2 29 36 2 32 39))
+
+;; 5.4.1.3 Fragments on composite types
+(deftest-invalid fragment-on-composite-type-2 example-schema
+  "fragment inlineFragmentOnScalar on Dog {
+     ... on Boolean {
+       somethingElse
+     }
+   }"
+  (err "inline fragment on non-composite type 'Boolean'" 2 13 53 2 20 60))
+
+(deftest-invalid inline-fragment-on-undeclared-type example-schema
+  "fragment inlineFragmentOnScalar on Dog {
+     ... on UndeclaredType {
+       somethingElse
+     }
+   }"
+  (err "inline fragment on undefined type 'UndeclaredType'" 2 13 53 2 27 67))
+
+;; 5.4.2.2 Fragment spreads must not form cycles
+(deftest-invalid fragment-spreads-must-not-form-cycles example-schema
+  "{ dog { ...nameFrag } }
+   fragment otherFrag on Dog {
+     name
+   }
+   fragment extraFrag on Dog {
+     ...nameFrag
+   }
+   fragment nameFrag on Dog {
+     name
+     ...barkVolumeFrag
+   }
+   fragment barkVolumeFrag on Dog {
+     barkVolume
+     ...nameFrag
+   }"
+  (err "fragment 'nameFrag' contains a cyclic reference" 8 13 135 8 21 143)
+  (err "fragment 'barkVolumeFrag' contains a cyclic reference" 12 13 203 12 27 217))
+
+(deftest-invalid fragment-spreads-must-not-form-cycles-2 example-schema
+  "{ dog { ...a } }
+   fragment a on Dog { name ...b }
+   fragment b on Dog { name ...c }
+   fragment c on Dog { name ...a }"
+  (err "fragment 'a' contains a cyclic reference" 2 13 29 2 14 30)
+  (err "fragment 'b' contains a cyclic reference" 3 13 64 3 14 65)
+  (err "fragment 'c' contains a cyclic reference" 4 13 99 4 14 100))
+
+(deftest-invalid fragment-undefined example-schema
+  "{ dog { ...nameFrag } }
+   fragment nameFrag on Dog {
+     ... undefinedFrag
+   }"
+  (err "fragment 'undefinedFrag' is not defined" 3 10 63 3 23 76))
+
+(deftest-invalid sec-5-2-1-fragment-field-not-defined example-schema
+  "{ dog }
+   fragment fieldNotDefined on Dog {
+     meowVolume
+   }"
+  (err "field 'meowVolume' is not defined on type 'Dog'" 3 6 50 3 16 60))
+
+;; TODO
+;; ;; 5.4.1.4 Fragments must be used
+;; (deftest-invalid fragment-must-be-used
+;;   "fragment nameFragment on Dog { # unused
+;;      name
+;;    }
+;;    {
+;;      dog {
+;;        name
+;;      }
+;;    }"
+;;   {})
+
+;; TODO: 5.4.2.3 fragment spread is possible
+;; TODO: 5.5.1 input object field uniqueness
+;; TODO: 5.6 directives
+;; TODO: 5.7 variables
+
