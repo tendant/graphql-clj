8e3ad79a8c1665c534d60ce94fa2e95f564f0720
Lei Wang <tendant@gmail.com>
2017-04-21 07:21:46 -0700

    wip: fix introspection for type resolver

--- src/graphql_clj/introspection.clj
+++ src/graphql_clj/introspection.clj
@@ -41,77 +41,102 @@
     (default-root-query-node root-query-name)))
 
 (defn type-kind [type]
-  (assert (:tag type) (format "type tag is nil for type: %s" type))
-  (assert (:name type) (format "type name is nil for type: %s" type))
-  (cond
-    (contains? #{'Int 'Float 'String 'Boolean 'ID} (:name type)) :SCALAR
-    (:name type) :OBJECT
-    :else (throw (ex-info (format "unknow type kind for type: %s" type) {:type type}))))
+  (if (:kind type)
+    (:kind type)
+    (do
+      (assert (:tag type) (format "type tag is nil for type: %s" type))
+      (let [tag (:tag type)
+            name (:name type)]
+        (cond
+          (:required type) :NON_NULL
+          (and (= :basic-type tag)
+               (contains? #{'Int 'Float 'String 'Boolean 'ID} name)) :SCALAR
+          (= :basic-type tag) :OBJECT
+          (= :list-type tag) :LIST
+          (= :type-definition tag) :OBJECT ; root query kind
+          (= :union-definition tag) :UNION
+          (= :interface-definition tag) :INTERFACE
+          (= :enum-definition tag) :ENUM
+          :else (assert nil (format "unknown type kind for type: %s" type)))))))
 
-(defn type-resolver [type]
-  (println "type-resolver:" type)
-  (when (not (contains? #{:LIST :NON_NULL} (:kind type)))
-    (assert (or (:name type) (:type-name type)) (format "type name is null for type: %s." type))
-    (assert (:kind type) (format "kind is nil for type: %s" type)))
-  (if (:required type)
-    ;; Wrap required type ast NON_NULL
-    {;; A Non-Null type cannot modify another Non-Null type.
-     :kind :NON_NULL
-     ;; defer resolving ofType
-     :inner-type (dissoc type :required)}
-    {:kind (:kind type)
-     :name (:name type)
-     :description (:description type)
-
-     ;; OBJECT and INTERFACE only
-     :fields (when (contains? #{:OBJECT :INTERFACE} (:kind type))
-               (:fields type)) ; defer resolving of fields
-
-     ;; OBJECT only
-     :interfaces [] ; TODO
-
-     ;; INTERFACE and UNION only
-     :possibleTypes nil
-
-     ;; ENUM only
-     :enumValues (when (= :ENUM (:kind type))
-                   (assert (:constants type) (format "enum constants is empty for type: %s." type))
-                   (:fields type))
-
-     ;; INPUT_OBJECT only
-     :inputFields (when (= :INPUT_OBJECT (:kind type))
-                    (assert (:fields type) (format "input fields is empty for type: %s." type))
-                    (:fields type))
-
-     ;; NON_NULL and LIST only
-     ;; :ofType nil
-     :inner-type (:inner-type type) ; provide inner-type for ofType resolver
-     }))
-
-(defn schema-types [schema]
-  (assert (map? (:type-map schema)) (format "schema has no :type-map: %s" schema))
-  (map type-resolver (vals (:type-map schema))))
+(defn field-kind [field]
+  (assert (:tag field) (format "field tag is nil for type: %s" field))
+  (assert (:name field) (format "field name is nil for type: %s" field))
+  (let [tag (:tag field)
+        name (:name field)
+        type (:type field)]
+    (cond
+      (= :type-field tag) (type-kind type)
+      :else (throw (ex-info (format "unknown field kind for field: %s" field) {:field field})))))
 
 (defn field-resolver [field]
   (assert (:name field)
           (format "field name is null for field: %s" field))
-  (assert (:kind field)
-          (format "field kind is null for field: %s" field))
-  (let [type-name (or (:type-name field) (get-in field [:type :name]))]
-    (assert type-name (format "field type-name is nil for field: %s" field))
+  (assert (:tag field)
+          (format "field tag is null for field: %s" field))
+  (let [type-name (or (:type-name field) (get-in field [:type :name]))
+        kind (field-kind field)
+        inner-type (get-in field [:type :inner-type])]
+    (assert kind (format "field kind is nil for field: %s" field))
+    (assert (or type-name inner-type) (format "field type-name and inner-type are both nil for field: %s" field))
     {:name (:name field)
      :description (:description field)
      :args (:arguments field) ; defer resolving of arguments
      
      ;; :type nil ; defer resolving of type
      :type-name type-name
-     :kind (:kind field)
-     :inner-type (:inner-type field)
+     :kind kind
+     :inner-type inner-type
      :required (:required field)
      
      :isDeprecated false ; TODO
      :deprecationReason nil ; TODO
      }))
+
+(defn type-resolver [type]
+  (println "type-resolver:" type)
+  (let [kind (type-kind type)]
+    (when (not (contains? #{:LIST :NON_NULL} kind))
+      (assert (or (:name type) (:type-name type)) (format "type name is null for type: %s." type))
+      (assert kind (format "kind is nil for type: %s" type)))
+    (if (:required type)
+      ;; Wrap required type ast NON_NULL
+      {;; A Non-Null type cannot modify another Non-Null type.
+       :kind :NON_NULL
+       ;; defer resolving ofType
+       :inner-type (dissoc type :required)}
+      {:kind kind
+       :name (:name type)
+       :description (:description type)
+
+       ;; OBJECT and INTERFACE only
+       :fields (when (contains? #{:OBJECT :INTERFACE} kind)
+                 (:fields type)) ; defer resolving of fields
+
+       ;; OBJECT only
+       :interfaces [] ; TODO
+
+       ;; INTERFACE and UNION only
+       :possibleTypes nil
+
+       ;; ENUM only
+       :enumValues (when (= :ENUM kind)
+                     (assert (:constants type) (format "enum constants is empty for type: %s." type))
+                     (:fields type))
+
+       ;; INPUT_OBJECT only
+       :inputFields (when (= :INPUT_OBJECT kind)
+                      (assert (:fields type) (format "input fields is empty for type: %s." type))
+                      (:fields type))
+
+       ;; NON_NULL and LIST only
+       ;; :ofType nil
+       :inner-type (:inner-type type) ; provide inner-type for ofType resolver
+       })))
+
+(defn schema-types [schema]
+  (assert (map? (:type-map schema)) (format "schema has no :type-map: %s" schema))
+  (map type-resolver (vals (:type-map schema))))
 
 (defn input-field-resolver [field]
   (assert (:field-name field) (format "field name is null for input value: %s." field))
