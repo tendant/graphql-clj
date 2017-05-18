637efd80ba3b1b88b946c6ee4cc5101ca345568f
Lei Wang <tendant@gmail.com>
2017-05-18 14:13:23 -0700

    use oracle jdk8

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["java"]
-  ;; :javac-options     ["-target" "1.8" "-source" "1.8"]
+  :javac-options     ["-target" "1.8" "-source" "1.8"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [instaparse "1.4.3"]
                  [org.clojure/core.match "0.3.0-alpha4"]
